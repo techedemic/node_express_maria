0fe6755eec184f7163ba8cc6933b893e3d87bf23
techedemic <hendri@techedemic.com>
2021-03-08 16:33:16 +0200

    Added .gitignore

--- routes/user.js
+++ routes/user.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const pool = require('../helpers/database');
+const bcrypt = require('bcrypt');
 
 
 router.get('/:id', async function(req,res){
@@ -19,11 +20,31 @@ router.get('/:id', async function(req,res){
 router.post('/register', async function(req,res) {
     try {
         const {email, password} = req.body;
+        
+        const encryptedPassword = await bcrypt.hash(password,10)
 
         const sqlQuery = 'INSERT INTO user (email, password) VALUES (?,?)';
-        const result = await pool.query(sqlQuery, [email, password]);
+        const result = await pool.query(sqlQuery, [email, encryptedPassword]);
 
         res.status(200).json({userId: result.insertId});
+    } catch (error) {
+        res.status(400).send(error.message)
+    }
+})
+
+router.post('/login', async function(req,res) {
+    try {
+        const {id,password} = req.body;
+
+        const sqlGetUser = 'SELECT password FROM user WHERE id=?';
+        const rows = await pool.query(sqlGetUser,id);
+        if(rows){
+            
+            const isValid = await bcrypt.compare(password,rows[0].password)
+            res.status(200).json({valid_password: isValid});
+        }
+        res.status(200).send(`User with id ${id} was not found`);
+        
     } catch (error) {
         res.status(400).send(error.message)
     }
